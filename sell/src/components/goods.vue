fef939b3ac4921581ddd5cf8aae649e6dcb71c6f
ruoshuidu <1909630302@qq.com>
2018-05-23 21:59:02 +0800

    better-scroll

--- sell/src/components/goods.vue
+++ sell/src/components/goods.vue
@@ -1,13 +1,245 @@
 <template>
-    <div class="goods">
-        goods111
+    <div id="goods">
+      <div class="menu-wrapper" ref="menuWrapper">
+          <div v-for="(item,index) in goods" 
+                :key="index" class="menu-item" 
+                :class="{current:currentIndex === index}" 
+                @click="selectIndex(index,$event)"
+          >
+              <div class="text-wrapper">
+                 <span class="icon" v-show="item.type > 0" :class="classMap[item.type]"></span>
+                 <span class="text">{{item.name}}</span>
+              </div>
+          </div>
+      </div>
+      <div class="foods-wrapper" ref="foodWrapper">
+        <ul>
+            <li v-for="(items,index) in goods" :key="index" class="foods-list">
+                <div class="items-title">{{items.name}}</div>
+                <ul class="items-content">
+                    <li v-for="(item2,index) in items.foods" :key="index" class="items-content-wrapper">
+                        <div class="item-icon"><img :src="item2.icon" alt=""></div>
+                        <div class="item-content">
+                            <div class="item-name">{{item2.name}}</div>
+                            <div v-show="item2.description" class="items-description">
+                                {{item2.description}}
+                            </div>
+                            <div class="rating">
+                                <span>月售{{item2.sellCount}}份</span>
+                                <span style="margin-left:12px;">好评率{{item2.rating}}%</span>
+                            </div>
+                            <div class="prices">
+                                <span class="price">{{item2.price}}</span>
+                                <span class="old-price" v-if="item2.oldPrice" style="margin-left:8px;">￥{{item2.oldPrice}}</span>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </li>    
+        </ul>    
+      </div>  
+      <!-- 购物车 -->
+      
     </div>
 </template>
 <script>
+import axios from 'axios'
+import Vue from 'vue'
+import BScroll from 'better-scroll'
 export default {
-    
+    data () {
+        return {
+            goods:[],
+            heightList:[]
+        }
+    },
+    created () {
+        this.classMap = ['decrease','discount','special','invoice','guarantee']
+        axios.get('/good/goods').then(res => {
+           this.goods = res.data.data 
+           Vue.nextTick(() => {
+               this._initScroll()//初始化scroll
+               this._caculateHeight()//计算每一个foods-list高度
+           })          
+        })    
+    },
+    computed:{
+        currentIndex() {
+            for(let i = 0; i < this.heightList.length; i ++){
+                let height1 = this.heightList[i]
+                let height2 = this.heightList[i + 1]
+                if(!height2 || (this.scrollY >= height1 && this.scrollY <= height2)){
+                    return i
+                }
+                console.log(height1 + ' ' + height2 + ' ' + i)
+            }
+            return 0
+        }
+    },
+    methods:{
+        selectIndex($index,$event) {
+            if(!$event._constructed){
+                return
+            }
+            let foodsList = this.$refs.foodWrapper.getElementsByClassName('foods-list')
+            this.foodsScroll.scrollToElement(foodsList[$index],300)
+        },
+        _initScroll() {
+            this.menuScroll = new BScroll(this.$refs.menuWrapper,{
+                click:true
+            })
+            this.foodsScroll = new BScroll(this.$refs.foodWrapper,{
+                probeType:3,
+                click:true
+            })
+            //获取鼠标y值
+            this.foodsScroll.on('scroll',(pos) => {
+              this.scrollY = Math.abs(Math.round(pos.y))
+            })
+        },
+        _caculateHeight() {
+           let foodsList = this.$refs.foodWrapper.getElementsByClassName('foods-list')
+           let height = 0;
+           this.heightList.push(height)
+           for(let i = 0; i < foodsList.length; i ++){
+               height += foodsList[i].clientHeight
+               this.heightList.push(height)
+           }
+        }
+    }
 }
 </script>
-<style>
-
+<style lang="less" scoped>
+@import '../../static/style.css';
+#goods{
+    display: flex;
+    position: absolute;
+    top: 174px;
+    bottom: 48px;
+    left: 0;
+    right: 0;
+    width: 100%;
+    overflow: hidden;
+    .menu-wrapper{
+        flex:0 0 80px;
+        width: 80px;
+        background: #f3f5f7;
+        .menu-item{
+            padding: 0 12px;
+            font-size: 0;
+            border-bottom: 1px solid rgba(7,17,27,0.1);
+            
+            .text-wrapper{
+                display: table-cell;
+                vertical-align: middle;
+                height: 54px;
+                .icon{
+                    display: inline-block;
+                    vertical-align: top;
+                    width: 16px;
+                    height: 16px; 
+                    &.special{
+                        background: url('../assets/img/special_2@2x.png') no-repeat;
+                        background-size: 16px 16px;  
+                    }   
+                    &.discount{
+                        background: url('../assets/img/discount_2@2x.png') no-repeat;
+                        background-size: 16px 16px;  
+                    }  
+                    &.decrease{
+                        background: url('../assets/img/decrease_2@2x.png') no-repeat;
+                        background-size: 16px 16px;  
+                    }
+                    &.invoice{
+                        background: url('../assets/img/invoice_2@2x.png') no-repeat;
+                        background-size: 16px 16px;  
+                    }
+                    &.guarantee{
+                        background: url('../assets/img/guarantee_2@2x.png') no-repeat;
+                        background-size: 16px 16px;  
+                    }                               
+                }   
+                .text{
+                    font-weight:200;
+                    font-size: 12px;
+                    width: 56px;
+                    line-height: 14px;
+                }   
+            }
+        }
+        .current{
+            background: #fff;
+            &:after{
+                border-top: 1px solid #fff;
+                font-size: 12px;
+            }
+            .text{
+                font-weight: bold;
+            }
+        }
+    }
+    .foods-wrapper{
+       flex: 1;
+       background: #fff;
+       .foods-list{
+           .items-title{
+               width: 100%;
+               background: #f3f5f7;
+               padding: 0 18px;
+               height: 26px;
+               font-size:12px;
+               color: rgb(147,153,159);
+               line-height: 26px;
+               border-left: 4px solid #d9dde1;
+           }
+           .items-content{
+               .items-content-wrapper{
+                   display: flex;
+                   margin:18px;
+                   position: relative;
+                   background: #fff;
+                   padding-bottom: 18px;
+                   border-bottom: 1px solid #d9dde1;
+                   &:last-child{
+                       border: 1px solid #fff;
+                       margin-bottom: 0;
+                   }
+                   .item-icon{
+                       flex: 0 0 57px;
+                       img{
+                            width: 57px;
+                            height: 57px;
+                       }
+                   }
+                   .item-content{
+                       flex: 1;
+                       padding-left: 10px;
+                       .item-name{
+                           font-size: 14px;
+                           color:rgb(7,17,27);
+                       }
+                       .items-description,.rating{
+                           color: rgb(147,153,159);
+                           font-size: 10px;
+                           margin: 8px 0 0 0;
+                       }
+                       .prices{
+                           font-size: 20px;
+                           color: rgb(240,20,20);
+                           font-weight: 700;
+                           margin-top:6px;
+                           .old-price{
+                               font-size: 12px;
+                               color:rgb(147,153,159);
+                               vertical-align: top;
+                               text-decoration: line-through;
+                           }
+                       }
+                   }
+               }
+           }
+       }
+    }
+}
 </style>
+
--- sell/src/components/header.vue
+++ sell/src/components/header.vue
@@ -7,14 +7,29 @@
             <div class="content">
                 <div class="title">
                     <span class="brand "></span>
-                    <span>{{seller.name}}</span>
+                    <span class="name">{{seller.name}}</span>
+                </div>
+                <div class="desc">
+                    {{seller.description}} / {{seller.deliveryTime}}分钟送达
+                </div>
+                <div class="supports" v-if="seller.supports">
+                    <span class="icon" :class="classMap[seller.supports[0].type]"></span>
+                    <span>{{seller.supports[0].description}}</span>
+                </div>
+                <div class="num-wrapper" v-if="seller.supports">
+                    <span class="number">{{seller.supports.length}}个</span>
+                    <span class="icon-keyboard_arrow_right"></span>
                 </div>
             </div>
         </div>
         <div class="buttetin-wrapper">
-            
+            <span class="butterin-icon"></span>
+            <span class="butterin">{{seller.bulletin}}</span>
+             <span class="icon-keyboard_arrow_right more"></span>
         </div>
-        <div class="bg"></div>
+        <div class="bg">
+            <img :src="seller.avatar" alt="">
+        </div>
     </div>
 </template>
 <script>
@@ -22,8 +37,9 @@ export default {
     props:{
         seller:Object,
     },
-   
+    
     created(){
+        this.classMap = ['decrease','discount','special','invoice','guarantee']
         console.log(this.seller.name)
     }
 }
@@ -48,7 +64,7 @@ export default {
         .content{
             margin-left: 80px;
             color: #fff;
-            font-weight: 900;
+           
             .title{
                 .brand{
                     display: inline-block;
@@ -58,7 +74,100 @@ export default {
                     background-size: 30px 18px;
                     vertical-align: top;
                 }
+                .name{
+                    font-size: 16px;
+                    font-weight: 900;
+                }
             }
+            .desc{
+                margin:8px 0 10px 0;
+                font-size: 12px;
+            }
+            .supports{
+                font-size: 12px;
+                span{
+                    display: inline-block;
+                }
+                .icon{
+                    vertical-align: top;
+                    width: 16px;
+                    height: 16px; 
+                    &.special{
+                        background: url('../assets/img/special_2@2x.png') no-repeat;
+                        background-size: 16px 16px;  
+                    }   
+                    &.discount{
+                        background: url('../assets/img/discount_2@2x.png') no-repeat;
+                        background-size: 16px 16px;  
+                    }  
+                    &.decrease{
+                        background: url('../assets/img/decrease_2@2x.png') no-repeat;
+                        background-size: 16px 16px;  
+                    }
+                    &.invoice{
+                        background: url('../assets/img/invoice_2@2x.png') no-repeat;
+                        background-size: 16px 16px;  
+                    }
+                    &.guarantee{
+                        background: url('../assets/img/guarantee_2@2x.png') no-repeat;
+                        background-size: 16px 16px;  
+                    }                                                 
+                }                 
+            }
+            .num-wrapper{
+                color: #fff;
+                position: absolute;
+                right: 12px;
+                bottom: 12px;
+                padding:7px 8px;
+                background-color:rgba(0,0,0,0.2);
+                border-radius: 12px;
+                font-size: 12px;
+                
+            }
+        }
+    }
+    .buttetin-wrapper{
+        position: relative;
+        height: 28px;
+        background-color: rgba(7,17,27,0.2);
+        padding:0 12px;
+        line-height: 28px;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        font-size: 10px;
+        color: #fff;
+        .butterin-icon{
+            display: inline-block;
+            vertical-align:middle;
+            width: 22px;
+            height: 12px;
+            background: url('../assets/img/bulletin@2x.png');
+            background-size: 22px 12px;
+            margin-right: 4px;
+        }
+        .butterin{
+            vertical-align: middle;
+        }
+        .more{
+            position: absolute;
+            right: 8px;
+            top:10px ;
+            font-size: 12px;
+        }
+      
+    }
+    .bg{
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: -1;
+        width: 100%;
+        filter:blur(10px);
+        img{
+            background-size: cover;
+            width: 100%;
         }
     }
 }
